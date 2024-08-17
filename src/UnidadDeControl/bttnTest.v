0de7e6a5db9e5f75b3b79bd1be047743530ba495
JuanDavidPalaciosCh <juandapalaciosch@gmail.com>
2024-08-17 17:49:17 -0500

    Se arregla la funcionalidad del botón Test junto a su TB

--- src/UnidadDeControl/bttnTest.v
+++ src/UnidadDeControl/bttnTest.v
@@ -3,57 +3,106 @@ module bttnTest #(parameter COUNT_MAX = 25000000,FiveSegs = 10)(
     input botonTest,
     input clk,
     input rst,
-    output reg btnTest
+    output reg btnTest,
+    output reg [3:0] contBtnPress
 );
 
-    reg [$clog2(COUNT_MAX)-1:0] counter;
-    reg [3:0] contmsegs;
-    reg clkmseg;
+localparam PRESSING = 3'd0; // 0
+localparam WAITING = 3'd1;   // 1
+localparam COUNTING = 3'd2;   // 2
 
-    always @(posedge clkmseg or posedge rst) begin
-        if(rst)begin
+reg [3:0] state;
+reg [3:0] next;
+
+reg [$clog2(COUNT_MAX)-1:0] counter;
+reg [3:0] contmsegs;
+reg clkmseg;
+
+reg prevBotonTest;
+
+initial begin
+    state <= PRESSING;
+    next <= PRESSING;
+end
+
+//Reset de la máquina de estados
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        state <= PRESSING;
+    end else begin
+        state <= next;
+    end
+end
+
+// Máquina de Estados , general: Cambio entre estados
+always @(posedge clk or posedge rst) begin
+    case(state)
+        PRESSING: begin
             btnTest <= 0;
-        end else begin
+            contBtnPress <= 0;
             if (botonTest) begin
+                next <= WAITING;
+                counter <= 0;
                 contmsegs <= 0;
-                while (botonTest && (contmsegs <= FiveSegs)) begin
-                        if (contmsegs == FiveSegs) begin
-                            btnTest <= 1;
-                        end else 
-                            btnTest <= 0;
-                    end
-                end
             end else begin
-                btnTest <= 0;
+                next <= PRESSING;
+            end
+        end
+        WAITING: begin
+            if (!botonTest) begin
+                next <= PRESSING;
+            end else begin
+                if (contmsegs < FiveSegs) begin
+                    next <= WAITING;
+                end else begin
+                    btnTest <= 1;
+                    contmsegs <= 0;
+                    prevBotonTest <= botonTest;
+                    next <= COUNTING;
+                end
+            end
+        end
+        COUNTING: begin
+            btnTest <= 0;
+            if (contmsegs < FiveSegs) begin
+                next = COUNTING;
+            end else begin
+                next <= PRESSING;
             end
 
+            prevBotonTest <= botonTest;
+            if (botonTest && !prevBotonTest) begin
+                contBtnPress <= contBtnPress + 1;
+            end
         end
-        
+    endcase
+end
 
 
-    // Divisor de frecuencia , a reloj en medio segundo 
-		always @(posedge clk or posedge rst) begin
-		if(rst)begin
-			clkmseg <=0;
-			counter <=0;
-		end else begin
-		if (counter == COUNT_MAX-1) begin
-			clkmseg <= ~clkmseg;
-			counter <= 0;
-			end else begin
-				counter = counter +1;
-			end
-		end
-	end
+// Divisor de frecuencia , a reloj en s
+always @(posedge clk or posedge rst) begin
+if(rst)begin
+    clkmseg <=0;
+    counter <=0;
+end else begin
+if (counter == COUNT_MAX-1) begin
+    clkmseg <= ~clkmseg;
+    counter <= 0;
+    end else begin
+        counter = counter +1;
+    end
+end
+end
 
-// Contador de tiempo en general cada medio segundo
-	always @(posedge clkmseg or posedge rst) begin
-		if(rst)begin
-			contmsegs <= 0;
-		end else begin
-            contmsegs <= contmsegs+1;
-        end
-	end
-    
+// Contador de tiempo en general 
+always @(posedge clkmseg or posedge rst) begin
+if(rst)begin
+    contmsegs <= 0;
+end else begin
+    contmsegs <= contmsegs+1;
+end
+end
+
+
 
 endmodule
---
+++ src/UnidadDeControl/bttnTestTB.v
@@ -0,0 +1,76 @@
+`timescale 1ns / 1ps
+`include "/home/jpalaciosch/Desktop/Digital_I/DigitalLabs/proyecto-final/entrega-1-proyecto-grupo01-2024-1/src/UnidadDeControl/bttnTest.v"
+
+module bttnTestTB;
+
+    // Inputs
+    reg clk;
+    reg rst;
+    reg botonTest;
+    wire btnTest;
+    wire contBtnPress;
+
+    // Instantiate the module under test
+    bttnTest #(1, 5) uut(
+        .clk(clk),
+        .rst(rst),
+        .botonTest(botonTest),
+        .btnTest(btnTest)
+    );
+
+    // Clock generation
+	always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        botonTest = 0;
+        #5
+        rst = 1;
+        #10 rst = 0;
+
+        #10 botonTest = 1;
+        #10 botonTest = 0;
+
+        #10 botonTest = 1;
+        #10 botonTest = 0;
+
+        #10 botonTest = 1;
+        #10 botonTest = 0;
+
+        #10 botonTest = 1;
+        #110 botonTest = 0;
+
+        #30 botonTest = 1;
+        #10 botonTest = 0;
+
+        #10 botonTest = 1;
+        #10 botonTest = 0;
+
+        #10 botonTest = 1;
+        #10 botonTest = 0;
+
+        #50 botonTest = 1;
+        #110 botonTest = 0;
+
+        #30 botonTest = 1;
+        #10 botonTest = 0;
+
+        #10 botonTest = 1;
+        #10 botonTest = 0;
+
+        #10 botonTest = 1;
+        #10 botonTest = 0;
+
+        #10 botonTest = 1;
+        #10 botonTest = 0;
+
+    end
+
+    initial begin:TEST_CASE
+    $dumpfile("bttnTestTB.vcd");
+	$dumpvars(-1, uut);
+	#1000 $finish; 
+end
+
+
+endmodule
